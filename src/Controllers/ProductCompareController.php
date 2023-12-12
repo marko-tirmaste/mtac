2d88c497aede75e6835c5503624196b151d610f4
Marko Tirmaste <marko.tirmaste@gmail.com>
2023-12-12 21:16:19 +0200

    feat: m-tac and woocommerce product compare

---
+++ src/Controllers/ProductCompareController.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Controller class for comparing M-Tac and WooCommerce products.
+ * 
+ * @author Web Design Agency OÜ <info@vdisain.ee>
+ * @package Seeru\Mtac\Controllers
+ * @since 1.0.0
+ */
+
+declare(strict_types=1);
+
+namespace Seeru\Mtac\Controllers;
+
+defined('VDAI_PATH') or die;
+
+use Seeru\Mtac\Repositories\ProductRepository;
+use Seeru\Mtac\Services\ProductService;
+use WP_REST_Request;
+use WP_REST_Response;
+
+/**
+ * Controller class for comparing M-Tac and WooCommerce products.
+ * 
+ * @package Seeru\Mtac\Controllers
+ * @since 1.0.0
+ */
+class ProductCompareController
+{
+    public function __construct(
+        protected ProductRepository $productRepository,
+        protected ProductService $productService
+    ) {
+        //
+    }
+
+    public function index(WP_REST_Request $request): WP_REST_Response
+    {
+        $wooProducts = $this->productRepository->allMtacProducts();
+        $mtacProducts = $this->productService->get();
+
+        $map = $mtacProducts->map(function (array $mtacProduct) use ($wooProducts): array {
+            $wooProduct = $wooProducts
+                ->filter(fn(object $wooProduct): bool => (string) $wooProduct->mtac_id === (string) $mtacProduct['id'])
+                ->first();
+
+            return [
+                'id' => $wooProduct->id ?? null,
+                'mtac_id' => $mtacProduct['id'],
+                'title' => $wooProduct->title ?? $mtacProduct['title'],
+                'status' => $wooProduct->status ?? null,
+                '_links' => [
+                    'import' => rest_url("vdisain-interfaces/mtac/product/{$mtacProduct['id']}/import"),
+                    'update' => !empty($wooProduct->id) ? rest_url("vdisain-interfaces/mtac/product/{$wooProduct->id}/update") : null,
+                ],
+            ];
+        });
+
+        $wooProducts->each(function (object $wooProduct) use ($map): void {
+            if ($map->where('id', $wooProduct->id)->isEmpty()) {
+                $map->push([
+                    'id' => $wooProduct->id,
+                    'sku' => $wooProduct->mtac_id,
+                    'title' => $wooProduct->title,
+                    'status' => $wooProduct->status,
+                    '_links' => [
+                        'import' => rest_url("vdisain-interfaces/mtac/product/{$wooProduct->mtac_id}/import"),
+                        'update' => rest_url("vdisain-interfaces/mtac/product/{$wooProduct->id}/update"),
+                    ],
+                ]);
+            }
+        });
+
+        if ($request->has_param('diff')) {
+            $map = $map->filter(fn(array $product): bool => empty($product['id']) || empty($product['mtac_id']));
+        }
+
+        return new WP_REST_Response([
+            ...$map,
+        ]);
+    }
+}
---
+++ src/Repositories/ProductRepository.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Repository class for handling WooCommerce product records
+ * 
+ * @author Web Design Agency <info@vdisain.ee>
+ * @package Seeru\Mtac\Repositories
+ * @since 1.0.0
+ */
+
+declare(strict_types=1);
+
+namespace Seeru\Mtac\Repositories;
+
+use Vdisain\Plugins\Interfaces\Repositories\ProductRepository as BaseProductRepository;
+use Vdisain\Plugins\Interfaces\Support\Collection;
+
+/**
+ * Repository class for handling WooCommerce product records
+ * 
+ * @package Seeru\Mtac\Repositories
+ * @since 1.0.0
+ */
+class ProductRepository extends BaseProductRepository
+{
+    /**
+     * Gets all M-Tac products in WooCommerce.
+     * 
+     * @return \Vdisain\Plugins\Interfaces\Support\Collection
+     */
+    public function allMtacProducts(): Collection
+    {
+        /** @global \wpdb $wpdb */
+        global $wpdb;
+
+        return vi_collect(
+            $wpdb->get_results(
+                "SELECT 
+                    `{$wpdb->postmeta}`.`post_id` AS `id`,
+                    `{$wpdb->postmeta}`.`meta_value` AS `mtac_id`,
+                    `{$wpdb->posts}`.`post_title` AS `title`,
+                    `{$wpdb->posts}`.`post_status` AS `status`
+                FROM 
+                    `{$wpdb->postmeta}` 
+                    LEFT JOIN `{$wpdb->posts}` ON `{$wpdb->postmeta}`.`post_id` = `{$wpdb->posts}`.`ID`
+                WHERE 
+                    `{$wpdb->postmeta}`.`meta_key` = '_mtac_id';"
+            )
+        );
+    }
+
+    /**
+     * Gets all M-Tac products in WooCommerce that are not trashed.
+     * 
+     * @return \Vdisain\Plugins\Interfaces\Support\Collection
+     */
+    public function allMtacProductsNotTrashed(): Collection
+    {
+        /** @global \wpdb $wpdb */
+        global $wpdb;
+
+        return vi_collect(
+            $wpdb->get_results(
+                "SELECT 
+                    `{$wpdb->postmeta}`.`post_id` AS `id`,
+                    `{$wpdb->postmeta}`.`meta_value` AS `mtac_id`,
+                    `{$wpdb->posts}`.`post_title` AS `title`
+                FROM 
+                    `{$wpdb->postmeta}` 
+                    LEFT JOIN `{$wpdb->posts}` ON `{$wpdb->postmeta}`.`post_id` = `{$wpdb->posts}`.`ID`
+                WHERE 
+                    `{$wpdb->postmeta}`.`meta_key` = '_mtac_id'
+                    AND `{$wpdb->posts}`.`post_status` != 'trash';"
+            )
+        );
+    }
+
+    /**
+     * Gets all M-Tac products in WooCommerce that are not out of stock.
+     * 
+     * @return \Vdisain\Plugins\Interfaces\Support\Collection
+     */
+    public function allMtacProductsNotOutOfStock(): Collection
+    {
+        /** @global \wpdb $wpdb */
+        global $wpdb;
+
+        return vi_collect(
+            $wpdb->get_results(
+                "SELECT 
+                    `mtac`.`post_id` AS `id`,
+                    `mtac`.`meta_value` AS `mtac_id`,
+                    `{$wpdb->posts}`.`post_title` AS `title`
+                FROM 
+                    `{$wpdb->postmeta}` AS `mtac` 
+                    LEFT JOIN `{$wpdb->posts}` ON `mtac`.`post_id` = `{$wpdb->posts}`.`ID`
+                    LEFT JOIN `{$wpdb->postmeta}` AS `stock` ON `mtac`.`post_id` = `stock`.`post_id` AND `stock`.`meta_key` = '_stock'
+                WHERE 
+                    `mtac`.`meta_key` = '_mtac_id'
+                    AND `stock`.`meta_value` > 0
+                    AND `{$wpdb->posts}`.`post_status` != 'trash';"
+            )
+        );
+    }
+}
